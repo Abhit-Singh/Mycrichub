be33e0fd146973ac9fae719f3aabcf9bf2cff766
Abhit-Singh <110076459+Abhit-Singh@users.noreply.github.com>
2023-02-01 08:46:49 -0800

    populate user data

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,3 +1,22 @@
-const users = [];
+import bcrypt from "bcrypt";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "test1",
+    email: "test1@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "test2",
+    email: "test2@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
 
 export default users;
