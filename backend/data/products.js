5a76742e1252e00f4da2cb818bf2b22795f11c1c
Abhit-Singh <110076459+Abhit-Singh@users.noreply.github.com>
2023-02-01 08:46:00 -0800

    populate products

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,3 +1,64 @@
-const products = [];
+const products = [
+    {
+        name: "SS Match Cricket Batting Leg Guard Pads Mens Size",
+        image: "/images/leg_gaurd.jpg",
+        description:
+          "\
+          * High density sponge filled bolster.\
+          * Modern Moulded Minimalistic Design.\
+          * SS Match - Light Weight Premium Quality Batting Leg Guard Pads..",
+        brand: "SS",
+        category: "pads",
+        price: 45.0,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 12,
+      },
+      {
+        name: "Gortonshire Focus Red Cricket Batting Gloves Mens Size",
+        image: "/images/gloves.jpg",
+        description:
+          "\
+          * Top quality imported Polyurethane finger rolls. .\
+          * Nylon gusset for ventilation. .\
+          * Sponge-padded polyurethane back for better fit and comfort. ",
+        brand: "Gortonshire",
+        category: "gloves",
+        price: 30.0,
+        countInStock: 10,
+        rating: 0.0,
+        numReviews: 12,
+      },
+      {
+        name: "Master 500 English Willow Cricket Bat Size SH",
+        image: "/images/bat.jpg",
+        description:
+          "\
+          * Hand Selected Grade 4 English Willow.\
+          * Round Handle.\
+          * Specially designed Scale Grip of the Bat Handle- for complete bat control.",
+        brand: "SS",
+        category: "bats",
+        price: 125.0,
+        countInStock: 10,
+        rating: 0.0,
+        numReviews: 12,
+      },
+      {
+        name: "Gortonshire Match Cricket Ball Pink",
+        image: "/images/gloves.jpg",
+        description:
+          "\
+          *  Premium Quality Leather Ball.\
+          * Colour Pink .\
+          * Alum Tanned.",
+        brand: "Gortonshire",
+        category: "ball",
+        price: 5.0,
+        countInStock: 10,
+        rating: 0.0,
+        numReviews: 12,
+      },
+];
 
 export default products;
